dcb0405730b4376a9e5be035f43d1acced1bf61b
Fabien Cadet <fabic@wall.laptop>
2017-11-10 14:32:52 +0400

    hey.js : ok initial impl. find() files.

--- bin/hey.js
+++ bin/hey.js
@@ -25,43 +25,77 @@ function find(dir, regx) {
   let queue = [];
   let collect = [];
 
+  console.log("BEGIN: find('" + dir + "', " + regx + ").");
+
   queue.unshift({path: dir});
 
   while( queue.length > 0 ) {
 
     let {path} = queue.shift();
 
+    console.log(path + dirsep);
+
     let files = fs.readdirSync(path);
 
     files.forEach(file => {
-      let stats = fs.statSync(path + dirsep + file);
+      try {
+        const filePathName = path + dirsep + file;
 
-      let item = {path: path+dirsep+file, stats: stats};
+        let stats = fs.lstatSync(filePathName);
 
-      if (stats.isDirectory()) {
-        queue.unshift(item);
-        return;
-      }
-      else if (!stats.isFile()) {
-        return;
-      }
-      else if (!regx.test(file)) {
+        let item = {path: filePathName, stats: stats};
+
+        /* BFS: Enqueue directories for processing later on. */
+        if (stats.isDirectory()) {
+          queue.unshift(item);
+          console.log("  > " + item.path + dirsep);
           return;
+        }
+        /* We're _not_ following symlinks at the moment.
+         * TODO: Find out what to do with all those broken sym. links. */
+        else if (stats.isSymbolicLink()) {
+          try {
+            const target = fs.readlinkSync(item.path);
+            console.log("Symlink '" + item.path + "': " + target);
+          } catch(ex) {
+            console.log("ERROR: Couldn't read symlink '" + item.path + "',");
+            console.log("     ` RE-THROWING EXCEPTION: " + ex);
+            throw ex;
+          }
+          return;
+        }
+        /* Skip sockets, devices, or whatever else */
+        else if (!stats.isFile()) {
+          return;
+        }
+        else if (!regx.test(file)) {
+            return;
+        }
+
+        collect.push(item);
+
+        //console.log(item.path +' ('+ item.stats.size +')' );
+      } catch(ex) {
+        console.log("ERROR: Couldn't stat()" + ex);
+        return;
       }
-
-      collect.push(item);
-
-      // console.log(file +' '+ stats.size);
     });
 
   }
+
+  console.log("END: find('" + dir + "', " + regx + ").");
+  console.log("   ` Got " + collect.length + " files.");
 
   return collect;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-let files = find('/home/fabi/Downloads', /\.(mkv|mp4|avi|mpg|mpeg|mov|dv|divx|webm)$/);
+//const rootdir = "/mnt/nfs/wolf/papa/-- 10 -- MES RUSHS -- 990 GO";
+const rootdir = "/mnt/nfs/wolf/papa";
+let files = find(rootdir, /\.(avi|divx|dv|flv|ogm|mkv|mov|mp4|mpeg|mpg|xvid|webm)$/);
+
+console.log("Got " + files.length + " files. Sorting by size now.");
 
 files.sort((item1, item2) => {
   let sz = item1.stats.size - item2.stats.size;
@@ -71,5 +105,8 @@ files.sort((item1, item2) => {
 });
 
 files.forEach(({path, stats}) => {
-  console.log(stats.size + ' ' + path);
+  console.log(stats.nlink +' '+ stats.size + ' ' + path);
 });
+
+console.log("# <hardlinks count>  <file size>  <file path name>");
+console.log("Got " + files.length + " files. Sorted by size.");
