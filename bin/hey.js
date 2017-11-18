f8676930985e342fabe3d6909c81e4816ae0c41c
Fabien Cadet <fabi@wall.laptop>
2017-11-18 14:38:59 +0400

    WIP, forgot to pull minor stuff done this morning, grrr -_-

--- bin/hey.js
+++ bin/hey.js
@@ -14,7 +14,7 @@ cli.parse({
   time: [ 't', 'An access time', 'time', false],
   work: [ false, 'What kind of work to do', 'string', 'sleep' ],
   regx: [ 'r', 'Regular expression', 'string', 'TODO' ],
-}, ['find', 'encode']);
+}, ['find', 'hash', 'encode']);
 
 console.log(cli.args);
 console.log(cli.command);
@@ -38,12 +38,49 @@ files.sort((item1, item2) => {
     );
 });
 
-files.forEach(({path, stats}) => {
-  console.log(stats.nlink +' '+ stats.size + ' ' + path);
-});
+/// FIND ONLY
+if (cli.command == 'find') {
+  files.forEach(({path, stats}) => {
+    console.log(stats.nlink +' '+ stats.size + ' ' + path);
+  });
+  console.log("# <hardlinks count>  <file size>  <file path name>");
+  console.log("Got " + files.length + " files. Sorted by size.");
+}
+// HASH
+else if (cli.command == 'hash') {
+  let currentSet = [];
+  let currentSize = null;
+  for (let i=0; i<files.length; i++) {
+    const file = files[i];
 
-console.log("# <hardlinks count>  <file size>  <file path name>");
-console.log("Got " + files.length + " files. Sorted by size.");
+    if (file.stats.size == currentSize) {
+      currentSet.push( file );
+      continue;
+    }
+
+    // ( Else: )
+
+    if (currentSet.length == 0) {
+      cli.error('Huh!');
+      currentSet.push( file );
+      currentSize = file.stats.size;
+    }
+    else if (currentSet.length == 1) {
+      process.stderr.write('.');
+    }
+    else {
+      cli.info("Got one same-size set with " + currentSet.length + " files.");
+      currentSet.forEach(({path, stats}) => {
+        cli.info("  > " + stats.nlink +' '+ stats.size + ' ' + path);
+      });
+    }
+
+    // Rewind so that we start over with an empty files set.
+    //i--;
+    currentSize = file.stats.size;
+    currentSet = [ file ];
+  }
+} // if hash //
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
